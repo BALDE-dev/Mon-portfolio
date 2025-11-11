74801334b92f85dd26782b89fac89da0e72487ad
BALDE-dev <oumar.balde16@unchk.edu.sn>
2025-11-11 16:33:05 +0000

    Première version du portfolio

---
+++ script.js
@@ -0,0 +1,50 @@
+//  MENU EMPILE
+const boutonMenu = document.getElementById('bouton-menu');
+const liensNavigation = document.getElementById('liens-navigation');
+
+boutonMenu.addEventListener('click', () => {
+  liensNavigation.classList.toggle('actif');
+});
+
+//  NAVIGATION AU SCROLL
+document.addEventListener("DOMContentLoaded", () => {
+  const liensNav = document.querySelectorAll('.liens-navigation a');
+
+  liensNav.forEach(lien => {
+    lien.addEventListener('click', e => {
+      e.preventDefault(); 
+
+      const targetId = lien.getAttribute('href').substring(1);
+      const section = document.getElementById(targetId);
+
+      if (section) {
+        window.scrollTo({
+          top: section.offsetTop - 70, 
+          behavior: 'smooth'
+        });
+      }
+    });
+  });
+});
+
+//  FERMER LE MENU AU CLIC SUR UN LIEN 
+const liens = document.querySelectorAll('.liens-navigation a');
+
+liens.forEach(lien => {
+  lien.addEventListener('click', () => {
+    liensNavigation.classList.remove('actif');
+  });
+});
+
+// Animation cartes projets au scroll
+const cartesProjets = document.querySelectorAll('.carte-projet');
+
+const observerProjets = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('visible');
+    }
+  });
+}, { threshold: 0.3 });
+
+cartesProjets.forEach(carte => observerProjets.observe(carte));
